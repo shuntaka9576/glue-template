1b9a26ea1ca974659a526ef851dc619329e2b1cf
shuntaka9576 <shuntaka9576@gmail.com>
2024-04-04 08:15:05 +0900

    fix install rye method

--- setup-rye.sh
+++ setup-rye.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-RYE_VERSION=0.24.0
-curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" bash
+curl -sSf https://rye-up.com/get | RYE_VERSION=0.24.0 RYE_INSTALL_OPTION="--yes" bash
 source "$HOME/.rye/env"
 rye config --set-bool behavior.use-uv=true
